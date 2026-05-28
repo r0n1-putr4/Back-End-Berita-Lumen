2d5bc60e7457b893e56c6cf1ed88cc2a308a7bc3
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2026-05-28 17:30:35 +0700

    update code

--- storage/app/index.php
+++ storage/app/index.php
@@ -1,5 +1,16 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    http_response_code(200);
+    exit();
+}
+
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
